3be93d748fafca2e30af17a2d61f994260683acb
Joy-Jiayue <cheng2je@mail.uc.edu>
2024-07-24 23:04:05 +0800

    Update c_mymul.h

--- c_mymul.h
+++ c_mymul.h
@@ -1,5 +1,31 @@
 #include "encoding.h"
 
-require_extension(EXT_ZCA);
-WRITE_RD(sext_xlen(RVC_RS1 * RVC_RS2));  // 实现乘法操作
+// require_extension(EXT_ZCA);
+// WRITE_RD(sext_xlen(RVC_RS1 * RVC_RS2));  
+
+// c_mymul.h
+
+#ifndef _RISCV_C_MYMUL_H
+#define _RISCV_C_MYMUL_H
+
+#include "decode.h"
+
+reg_t rv32_c_mymul(processor_t* p, insn_t insn, reg_t pc) {
+
+    reg_t npc = sext_xlen(pc + insn_length(MATCH_C_MYMUL));
+    WRITE_RD(sext_xlen(RVC_RS1(p->get_state(), insn) * RVC_RS2(p->get_state(), insn)));
+    p->trace_opcode(MATCH_C_MYMUL, insn, pc, npc);
+    return npc;
+}
+
+reg_t rv64_c_mymul(processor_t* p, insn_t insn, reg_t pc) {
+
+    reg_t npc = sext_xlen(pc + insn_length(MATCH_C_MYMUL));
+    WRITE_RD(sext_xlen(RVC_RS1(p->get_state(), insn) * RVC_RS2(p->get_state(), insn)));
+    p->trace_opcode(MATCH_C_MYMUL, insn, pc, npc);
+    return npc;
+}
+
+#endif
+
 
