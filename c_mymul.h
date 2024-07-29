1ba72ea0fa968ad1b0d2106a7b55047c4c9b550e
Joy-Jiayue <cheng2je@mail.uc.edu>
2024-07-29 21:07:39 +0800

    Update c_mymul.h

--- c_mymul.h
+++ c_mymul.h
@@ -1,17 +1,13 @@
-#include "encoding.h"
-
-// require_extension(EXT_ZCA);
-// WRITE_RD(sext_xlen(RVC_RS1 * RVC_RS2));  
-
-// c_mymul.h
-
 #ifndef _RISCV_C_MYMUL_H
 #define _RISCV_C_MYMUL_H
 
 #include "decode.h"
+#include "encoding.h"  
+#include "processor.h" 
+#include "decode_macros.h"  
+#include "insn_template.h"
 
 reg_t rv32_c_mymul(processor_t* p, insn_t insn, reg_t pc) {
-
     reg_t npc = sext_xlen(pc + insn_length(MATCH_C_MYMUL));
     WRITE_RD(sext_xlen(RVC_RS1(p->get_state(), insn) * RVC_RS2(p->get_state(), insn)));
     p->trace_opcode(MATCH_C_MYMUL, insn, pc, npc);
@@ -19,13 +15,10 @@ reg_t rv32_c_mymul(processor_t* p, insn_t insn, reg_t pc) {
 }
 
 reg_t rv64_c_mymul(processor_t* p, insn_t insn, reg_t pc) {
-
     reg_t npc = sext_xlen(pc + insn_length(MATCH_C_MYMUL));
     WRITE_RD(sext_xlen(RVC_RS1(p->get_state(), insn) * RVC_RS2(p->get_state(), insn)));
     p->trace_opcode(MATCH_C_MYMUL, insn, pc, npc);
     return npc;
 }
 
-#endif
-
-
+#endif // _RISCV_C_MYMUL_H
